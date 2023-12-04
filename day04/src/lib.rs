cffff3dbcc5c144b288a7d72a4ee0df843e7a9a6
Andrew Howard <andrew@adh.me.uk>
2023-12-04 14:00:52 +0000

    Implemented day 4

---
+++ day04/src/lib.rs
@@ -0,0 +1,86 @@
+pub fn solve_part1(input: &str) -> usize {
+    input
+        .lines()
+        .filter_map(Card::try_parse)
+        .map(|card| {
+            let n = card.count_matches();
+            if n > 0 {
+                1 << (n - 1)
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+pub fn solve_part2(input: &str) -> usize {
+    let matches = input
+        .lines()
+        .filter_map(Card::try_parse)
+        .map(Card::count_matches)
+        .collect::<Vec<_>>();
+    let mut cards = matches.iter().map(|_| 1).collect::<Vec<usize>>();
+    matches.into_iter().enumerate().for_each(|(n, matches)| {
+        if matches > 0 {
+            let copies = cards[n];
+            (n + 1..=n + matches).for_each(|m| {
+                if let Some(s) = cards.get_mut(m) {
+                    *s += copies;
+                }
+            })
+        }
+    });
+    cards.into_iter().sum()
+}
+
+struct Card<'a> {
+    winning: &'a str,
+    numbers: &'a str,
+}
+
+impl<'a> Card<'a> {
+    fn try_parse(line: &'a str) -> Option<Self> {
+        let (_, rhs) = line.split_once(": ")?;
+        let (winning, numbers) = rhs.split_once(" | ")?;
+        Some(Self { winning, numbers })
+    }
+
+    fn count_matches(self) -> usize {
+        self.numbers
+            .split(' ')
+            .filter(|s| !s.is_empty() && self.winning.split(' ').any(|n| n == *s))
+            .count()
+    }
+}
+
+pub const EXAMPLE: &str = include_str!("../example.txt");
+pub const INPUT: &str = include_str!("../input.txt");
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        let result = solve_part1(EXAMPLE);
+        assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn part1_result() {
+        let result = solve_part1(INPUT);
+        assert_eq!(result, 23678);
+    }
+
+    #[test]
+    fn part2_example() {
+        let result = solve_part2(EXAMPLE);
+        assert_eq!(result, 30);
+    }
+
+    #[test]
+    fn part2_result() {
+        let result = solve_part2(INPUT);
+        assert_eq!(result, 15455663);
+    }
+}
---
+++ day04/src/main.rs
@@ -0,0 +1,6 @@
+use day04::{solve_part1, solve_part2, INPUT};
+use runner::run_solvers;
+
+fn main() {
+    run_solvers(solve_part1, solve_part2, INPUT);
+}
