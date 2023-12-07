a135bd47da027e815153e11e1d2fc9885a05de34
Andrew Howard <andrew@adh.me.uk>
2023-12-07 21:14:30 +0000

    Improved day 7 performance

--- day07/src/part2.rs
+++ day07/src/part2.rs
@@ -1,24 +1,23 @@
+use itertools::Itertools;
+
 pub fn solve_part2(input: &str) -> usize {
-    let mut hands = input
+    input
         .lines()
         .filter_map(Hand::try_parse)
-        .collect::<Vec<_>>();
-    hands.sort();
-    hands
-        .into_iter()
+        .sorted()
         .enumerate()
         .map(|(n, hand)| hand.bid * (n + 1))
         .sum()
 }
 
 #[derive(Eq, PartialEq)]
-struct Hand<'a> {
-    cards: HandType<'a>,
+struct Hand {
+    cards: HandType,
     bid: usize,
 }
 
-impl<'a> Hand<'a> {
-    fn try_parse(line: &'a str) -> Option<Self> {
+impl Hand {
+    fn try_parse(line: &str) -> Option<Self> {
         let (hand, bid) = line.split_once(' ')?;
         Some(Self {
             cards: HandType::from(hand),
@@ -27,147 +26,155 @@ impl<'a> Hand<'a> {
     }
 }
 
-impl<'a> PartialOrd for Hand<'a> {
+impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<'a> Ord for Hand<'a> {
+impl Ord for Hand {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         match (&self.cards, &other.cards) {
-            (HandType::FiveOfAKind(a), HandType::FiveOfAKind(b)) => cmp_cards(a, b),
+            (HandType::FiveOfAKind(a), HandType::FiveOfAKind(b)) => a.cmp(b),
             (HandType::FiveOfAKind(_), _) => core::cmp::Ordering::Greater,
             (_, HandType::FiveOfAKind(_)) => core::cmp::Ordering::Less,
 
-            (HandType::FourOfAKind(a), HandType::FourOfAKind(b)) => cmp_cards(a, b),
+            (HandType::FourOfAKind(a), HandType::FourOfAKind(b)) => a.cmp(b),
             (HandType::FourOfAKind(_), _) => core::cmp::Ordering::Greater,
             (_, HandType::FourOfAKind(_)) => core::cmp::Ordering::Less,
 
-            (HandType::FullHouse(a), HandType::FullHouse(b)) => cmp_cards(a, b),
+            (HandType::FullHouse(a), HandType::FullHouse(b)) => a.cmp(b),
             (HandType::FullHouse(_), _) => core::cmp::Ordering::Greater,
             (_, HandType::FullHouse(_)) => core::cmp::Ordering::Less,
 
-            (HandType::ThreeOfAKind(a), HandType::ThreeOfAKind(b)) => cmp_cards(a, b),
+            (HandType::ThreeOfAKind(a), HandType::ThreeOfAKind(b)) => a.cmp(b),
             (HandType::ThreeOfAKind(_), _) => core::cmp::Ordering::Greater,
             (_, HandType::ThreeOfAKind(_)) => core::cmp::Ordering::Less,
 
-            (HandType::TwoPair(a), HandType::TwoPair(b)) => cmp_cards(a, b),
+            (HandType::TwoPair(a), HandType::TwoPair(b)) => a.cmp(b),
             (HandType::TwoPair(_), _) => core::cmp::Ordering::Greater,
             (_, HandType::TwoPair(_)) => core::cmp::Ordering::Less,
 
-            (HandType::OnePair(a), HandType::OnePair(b)) => cmp_cards(a, b),
+            (HandType::OnePair(a), HandType::OnePair(b)) => a.cmp(b),
             (HandType::OnePair(_), _) => core::cmp::Ordering::Greater,
             (_, HandType::OnePair(_)) => core::cmp::Ordering::Less,
 
-            (HandType::HighCard(a), HandType::HighCard(b)) => cmp_cards(a, b),
+            (HandType::HighCard(a), HandType::HighCard(b)) => a.cmp(b),
         }
     }
 }
 
-fn cmp_cards(lhs: &str, rhs: &str) -> std::cmp::Ordering {
-    lhs.chars()
-        .zip(rhs.chars())
-        .find_map(|(lhs, rhs)| match (lhs, rhs) {
-            ('J', 'J') => None,
-            ('J', _) => Some(core::cmp::Ordering::Less),
-            (_, 'J') => Some(core::cmp::Ordering::Greater),
-
-            ('A', 'A') => None,
-            ('A', _) => Some(core::cmp::Ordering::Greater),
-            (_, 'A') => Some(core::cmp::Ordering::Less),
-
-            ('K', 'K') => None,
-            ('K', _) => Some(core::cmp::Ordering::Greater),
-            (_, 'K') => Some(core::cmp::Ordering::Less),
-
-            ('Q', 'Q') => None,
-            ('Q', _) => Some(core::cmp::Ordering::Greater),
-            (_, 'Q') => Some(core::cmp::Ordering::Less),
-
-            ('T', 'T') => None,
-            ('T', _) => Some(core::cmp::Ordering::Greater),
-            (_, 'T') => Some(core::cmp::Ordering::Less),
-
-            _ => match lhs.cmp(&rhs) {
-                std::cmp::Ordering::Equal => None,
-                x => Some(x),
-            },
-        })
-        .unwrap_or(core::cmp::Ordering::Equal)
-}
-
 #[derive(Eq, PartialEq)]
-enum HandType<'a> {
-    FiveOfAKind(&'a str),
-    FourOfAKind(&'a str),
-    FullHouse(&'a str),
-    ThreeOfAKind(&'a str),
-    TwoPair(&'a str),
-    OnePair(&'a str),
-    HighCard(&'a str),
+enum HandType {
+    FiveOfAKind(u32),
+    FourOfAKind(u32),
+    FullHouse(u32),
+    ThreeOfAKind(u32),
+    TwoPair(u32),
+    OnePair(u32),
+    HighCard(u32),
 }
 
-impl<'a> HandType<'a> {
-    fn from(hand: &'a str) -> Self {
-        let counts = count_cards(hand);
+impl HandType {
+    fn from(hand: &str) -> Self {
+        let (counts, cards_number) = count_cards(hand);
         let jokers = counts[0];
         match (counts.iter().skip(1).max(), jokers) {
             (_, 5) | (_, 4) | (Some(5), 0) | (Some(4), 1) | (Some(3), 2) | (Some(2), 3) => {
-                Self::FiveOfAKind(hand)
+                Self::FiveOfAKind(cards_number)
             }
-            (_, 3) | (Some(4), 0) | (Some(3), 1) | (Some(2), 2) => Self::FourOfAKind(hand),
+            (_, 3) | (Some(4), 0) | (Some(3), 1) | (Some(2), 2) => Self::FourOfAKind(cards_number),
             (Some(3), 0) => {
                 if counts.into_iter().any(|x| x == 2) {
-                    Self::FullHouse(hand)
+                    Self::FullHouse(cards_number)
                 } else {
-                    Self::ThreeOfAKind(hand)
+                    Self::ThreeOfAKind(cards_number)
                 }
             }
-            (Some(1), 2) => Self::ThreeOfAKind(hand),
+            (Some(1), 2) => Self::ThreeOfAKind(cards_number),
             (Some(2), 1) => {
                 if counts.into_iter().filter(|x| *x == 2).count() == 2 {
-                    Self::FullHouse(hand)
+                    Self::FullHouse(cards_number)
                 } else {
-                    Self::ThreeOfAKind(hand)
+                    Self::ThreeOfAKind(cards_number)
                 }
             }
             (Some(2), 0) => {
                 if counts.into_iter().filter(|x| *x == 2).count() == 2 {
-                    Self::TwoPair(hand)
+                    Self::TwoPair(cards_number)
                 } else {
-                    Self::OnePair(hand)
+                    Self::OnePair(cards_number)
                 }
             }
-            (Some(1), 1) => Self::OnePair(hand),
-            _ => Self::HighCard(hand),
+            (Some(1), 1) => Self::OnePair(cards_number),
+            _ => Self::HighCard(cards_number),
         }
     }
 }
 
 type CardCounts = [u8; 13];
 
-fn count_cards(hand: &str) -> CardCounts {
-    let mut result = [0; 13];
+fn count_cards(hand: &str) -> (CardCounts, u32) {
+    let mut counts = [0; 13];
+    let mut cards_number = 0;
     for card in hand.chars() {
+        cards_number *= 13;
         match card {
-            'J' => result[0] += 1,
-            'A' => result[1] += 1,
-            'K' => result[2] += 1,
-            'Q' => result[3] += 1,
-            'T' => result[4] += 1,
-            '9' => result[5] += 1,
-            '8' => result[6] += 1,
-            '7' => result[7] += 1,
-            '6' => result[8] += 1,
-            '5' => result[9] += 1,
-            '4' => result[10] += 1,
-            '3' => result[11] += 1,
-            '2' => result[12] += 1,
+            'J' => {
+                counts[0] += 1;
+            }
+            'A' => {
+                counts[1] += 1;
+                cards_number += 12;
+            }
+            'K' => {
+                counts[2] += 1;
+                cards_number += 11;
+            }
+            'Q' => {
+                counts[3] += 1;
+                cards_number += 10;
+            }
+            'T' => {
+                counts[4] += 1;
+                cards_number += 9;
+            }
+            '9' => {
+                counts[5] += 1;
+                cards_number += 8;
+            }
+            '8' => {
+                counts[6] += 1;
+                cards_number += 7;
+            }
+            '7' => {
+                counts[7] += 1;
+                cards_number += 6;
+            }
+            '6' => {
+                counts[8] += 1;
+                cards_number += 5;
+            }
+            '5' => {
+                counts[9] += 1;
+                cards_number += 4;
+            }
+            '4' => {
+                counts[10] += 1;
+                cards_number += 3;
+            }
+            '3' => {
+                counts[11] += 1;
+                cards_number += 2;
+            }
+            '2' => {
+                counts[12] += 1;
+                cards_number += 1;
+            }
             _ => panic!("unknown card: {}", card),
         }
     }
-    result
+    (counts, cards_number)
 }
 
 #[cfg(test)]
