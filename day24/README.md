cb9a2e6b10e90b579047b28ba8d48164b5659534
Andrew Howard <andrew@adh.me.uk>
2023-12-24 14:55:23 +0000

    Implemented day 24 part 1

---
+++ day24/README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2023 - Day 24
+
+<https://adventofcode.com/2023/day/24>
---
+++ day24/src/part1.rs
@@ -0,0 +1,103 @@
+use itertools::Itertools;
+
+pub fn solve(input: &str) -> usize {
+    count_collisions(input, 200000000000000.0, 400000000000000.0)
+}
+
+fn count_collisions(input: &str, min: f64, max: f64) -> usize {
+    input
+        .lines()
+        .map(Hailstone::parse)
+        .map(IntersectionCalculator::new)
+        .collect::<Vec<_>>()
+        .into_iter()
+        .tuple_combinations()
+        .filter_map(|(a, b)| {
+            a.intersection(&b)
+                .filter(|intersection| a.hailstone.is_in_future(*intersection))
+                .filter(|intersection| b.hailstone.is_in_future(*intersection))
+        })
+        .filter(|(x, y)| *x >= min && *x <= max && *y >= min && *y <= max)
+        .count()
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Hailstone {
+    px: i64,
+    py: i64,
+    vx: i64,
+    vy: i64,
+}
+
+impl Hailstone {
+    fn parse(line: &str) -> Self {
+        let (position, velocity) = line.split_once(" @ ").unwrap();
+        let mut position = position.split(',').map(str::trim).map(str::parse);
+        let px = position.next().unwrap().unwrap();
+        let py = position.next().unwrap().unwrap();
+        let mut velocity = velocity.split(',').map(str::trim).map(str::parse);
+        let vx = velocity.next().unwrap().unwrap();
+        let vy = velocity.next().unwrap().unwrap();
+        Self { px, py, vx, vy }
+    }
+
+    fn is_in_future(&self, intersection: (f64, f64)) -> bool {
+        let (x, _y) = intersection;
+        if self.vx < 0 {
+            (self.px as f64) > x
+        } else {
+            (self.px as f64) < x
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct IntersectionCalculator {
+    hailstone: Hailstone,
+    // (x - x') = (px - (px + vx)) = -vx
+    a: f64,
+    // (y - y') = (py - (py + vy)) = -vy
+    b: f64,
+    // (x*y' - y*x') = px*vy - py*vx
+    c: f64,
+}
+
+impl IntersectionCalculator {
+    fn new(hailstone: Hailstone) -> Self {
+        let a = hailstone.vx as f64 * -1.0;
+        let b = hailstone.vy as f64 * -1.0;
+        let c =
+            hailstone.px as f64 * hailstone.vy as f64 - hailstone.py as f64 * hailstone.vx as f64;
+        Self { hailstone, a, b, c }
+    }
+
+    fn intersection(&self, other: &Self) -> Option<(f64, f64)> {
+        let det = self.a * other.b - other.a * self.b;
+        if det == 0.0 {
+            return None;
+        }
+        let x = (self.c * other.a - self.a * other.c) / det;
+        let y = (self.c * other.b - self.b * other.c) / det;
+        Some((x, y))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::INPUT;
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../example.txt");
+
+    #[test]
+    fn example() {
+        let result = count_collisions(EXAMPLE, 7.0, 27.0);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn result() {
+        let result = solve(INPUT);
+        assert_eq!(result, 18651);
+    }
+}
