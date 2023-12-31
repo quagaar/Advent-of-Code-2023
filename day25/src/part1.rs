425627d70d182d7e9f6c8a4c3b3b9c8f65473b27
Andrew Howard <andrew@adh.me.uk>
2023-12-31 18:32:53 +0000

    Improved day 25 solution

--- day25/src/part1.rs
+++ day25/src/part1.rs
@@ -1,4 +1,3 @@
-use indicatif::ParallelProgressIterator;
 use itertools::Itertools;
 use pathfinding::prelude::components;
 use rayon::prelude::*;
@@ -18,9 +17,18 @@ pub fn solve(input: &str) -> usize {
     #[cfg(debug_assertions)]
     print_graph(&graph);
 
+    let graph_map = build_graph_hashmap(graph.iter().copied());
+
+    // Graph sorted by detour length (longest first)
+    // The edges with longest detour length are the ones that are most likely to split the graph
     let candidates: Vec<(&str, &str)> = graph
         .par_iter()
-        .map(|edge| (detour_length(edge, &graph), edge))
+        .map(|edge| {
+            (
+                detour_length(edge, &graph_map).expect("Single edge can split graph"),
+                edge,
+            )
+        })
         .collect::<Vec<_>>()
         .into_iter()
         .sorted_by_key(|(length, _)| *length)
@@ -28,30 +36,26 @@ pub fn solve(input: &str) -> usize {
         .map(|(_, edge)| *edge)
         .collect();
 
-    let count = graph.len() * graph.len() * graph.len() / 6;
-
+    // Brute-force search of all possible combinations of 3 edges
+    // Stops early when it finds a combination that splits the graph into two components
     (0..graph.len() - 2)
         .flat_map(|a| (a + 1..graph.len() - 1).map(move |b| (a, b)))
         .flat_map(|(a, b)| (b + 1..graph.len()).map(move |c| (a, b, c)))
-        .par_bridge()
-        .progress_count(count as u64)
-        .filter_map(|(a, b, c)| {
-            let groups = candidates
+        .find_map(|(a, b, c)| {
+            let groups = graph_map
                 .iter()
-                .enumerate()
-                .filter(|(i, _)| *i != a && *i != b && *i != c)
-                .fold(HashMap::new(), |mut graph, (_, component)| {
-                    graph
-                        .entry(component.0)
-                        .or_insert_with(|| vec![component.0])
-                        .push(component.1);
-                    graph
-                        .entry(component.1)
-                        .or_insert_with(|| vec![component.1])
-                        .push(component.0);
-                    graph
+                .map(|(node, linked)| {
+                    let mut linked = linked.clone();
+                    linked.retain(|x| {
+                        candidates[a] != (*node, *x)
+                            && candidates[a] != (*x, *node)
+                            && candidates[b] != (*node, *x)
+                            && candidates[b] != (*x, *node)
+                            && candidates[c] != (*node, *x)
+                            && candidates[c] != (*x, *node)
+                    });
+                    linked
                 })
-                .into_values()
                 .collect::<Vec<_>>();
             let groups = components(&groups);
             if groups.len() == 2 {
@@ -60,36 +64,43 @@ pub fn solve(input: &str) -> usize {
                 None
             }
         })
-        .find_any(|_| true)
         .unwrap()
 }
 
+// Build a hashmap of all the nodes and their neighbors
+fn build_graph_hashmap<'a>(
+    edges: impl Iterator<Item = (&'a str, &'a str)>,
+) -> HashMap<&'a str, Vec<&'a str>> {
+    edges.fold(HashMap::new(), |mut graph, (l, r)| {
+        graph.entry(l).or_insert_with(|| vec![l]).push(r);
+        graph.entry(r).or_insert_with(|| vec![r]).push(l);
+        graph
+    })
+}
+
 // Number of edges in the shortest path between two nodes that does not include the direct edge
-fn detour_length(edge: &(&str, &str), graph: &[(&str, &str)]) -> usize {
-    let mut queue = VecDeque::from(vec![(edge.0, 0)]);
+fn detour_length(
+    &(start, end): &(&str, &str),
+    graph_map: &HashMap<&str, Vec<&str>>,
+) -> Option<usize> {
+    let mut queue = VecDeque::from(vec![(start, 0)]);
     let mut visited = HashSet::new();
 
     while let Some((node, distance)) = queue.pop_front() {
         if visited.insert(node) {
-            let next = graph.iter().filter(|x| *x != edge).filter_map(|(l, r)| {
-                if *l == node {
-                    Some(r)
-                } else if *r == node {
-                    Some(l)
-                } else {
-                    None
+            for &next in graph_map.get(node).unwrap() {
+                if next == end {
+                    if node != start {
+                        return Some(distance + 1);
+                    }
+                } else if node != next {
+                    queue.push_back((next, distance + 1));
                 }
-            });
-            for node in next {
-                if *node == edge.1 {
-                    return distance + 1;
-                }
-                queue.push_back((node, distance + 1));
             }
         }
     }
 
-    0
+    None
 }
 
 #[allow(dead_code)]
@@ -115,9 +126,8 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "not done yet"]
     fn result() {
         let result = solve(INPUT);
-        assert_eq!(result, 42);
+        assert_eq!(result, 543036);
     }
 }
--- day25/src/part2.rs
+++ day25/src/part2.rs
@@ -1,25 +1,3 @@
-pub fn solve(input: &str) -> usize {
-    input.lines().count()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::super::INPUT;
-    use super::*;
-
-    const EXAMPLE: &str = include_str!("../example.txt");
-
-    #[test]
-    #[ignore = "not done yet"]
-    fn example() {
-        let result = solve(EXAMPLE);
-        assert_eq!(result, 42);
-    }
-
-    #[test]
-    #[ignore = "not done yet"]
-    fn result() {
-        let result = solve(INPUT);
-        assert_eq!(result, 42);
-    }
+pub fn solve(_input: &str) -> &'static str {
+    "Merry Christmas!"
 }
